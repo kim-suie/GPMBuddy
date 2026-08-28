2576c97c668c94380894c245c1abbb52ae16e844
kartikey-nerika72 <kartikeyk779@gmail.com>
2026-08-29 01:53:08 +0530

    write sevices files on fronted

---
+++ backend/src/models/faculty.js
@@ -0,0 +1,43 @@
+
+const mongoose = require("mongoose");
+
+const facultySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    department: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    designation: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Faculty", facultySchema);
